5d81b1e5ab510ffb58b6c179689de070933b7e13
LuminaryDev <redietgirmay@gmail.com>
2025-11-08 10:00:02 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+# import seaborn as sns
 import networkx as nx
 from pgmpy.models import DiscreteBayesianNetwork
 from pgmpy.estimators import BayesianEstimator
